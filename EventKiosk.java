7e0e90b47a42d93041c11765c80eda23350823a8
ANSHUL AGGARWAL <f2015048@goa.bits-pilani.ac.in>
2016-08-09 16:17:29 +0530

    [second commit doing kiosk]

--- EventKiosk.java
+++ EventKiosk.java
@@ -5,17 +5,21 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+import java.util.*;
+import java.time.*;
 public class EventKiosk
 {
     // instance variables - replace the example below with your own
-    private Event[] eventlist;
+    private ArrayList <Event> eventList = new ArrayList <Event>(50);
     
     private int noOfEvents,noOfRegistrations,noOfUsers;
     
     
-    private Registration[] registrationList;
+    private ArrayList<Registration> registrationList= new ArrayList<Registration>(500);
     
-    private User[] userList;
+    private ArrayList<User> userList = new ArrayList<User> (500);
+    
     
     public EventKiosk()
     {
@@ -26,23 +30,49 @@ public class EventKiosk
         
     }
     
-    private int addEvents( String username, Event[] events){
+    /*private int addEvents( String username, Event[] events){
         
-        // ?
-    }
+        
+    }*/
     
     int createEvent( String name, double cost)
     {
-        eventList.add(event(new Event(name,cost)));
+        eventList.add(new Event(name,cost) );
+        
+        
+        return eventList.size();
     }
     
-    private int createRegistration( String username, Event[] events)
+   
+    
+    
+    public int createUser(String name, String email, LocalDate dob)
     {
-    
-        // creates a regisration object
-    
+        userList.add(new User(name , email , dob));
+        return userList.size();
     }
     
-
+    public int registerUser(String username,Event[] events)
+    {
+        int i=0;
+        for( Registration register : registrationList)
+        {
+            if(register.getUserName().equals(username))
+            {   
+                break;
+            }
+            i++;
+        }
+        
+        return registrationList.get(i).addEvents(events);
+        //user= username;
+        
+    }
+    
+    /*
+    private int createRegistration(String username, Event[] events)
+    {
+       // Registration x= new Registration(username,events);
+    }*/
     
 }
